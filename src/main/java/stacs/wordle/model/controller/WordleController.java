b489bc4f002436e5909f06f716aff2f7eec869fd
poojagurubasappa <poojagurubasappa@gmail.com>
2023-02-07 23:11:27 +0000

    add main class implementation with MVC

--- src/main/java/stacs/wordle/model/controller/WordleController.java
+++ src/main/java/stacs/wordle/model/controller/WordleController.java
@@ -1,4 +1,13 @@
 package stacs.wordle.model.controller;
 
+import stacs.wordle.model.WordleGame;
+import stacs.wordle.model.view.WordleCLI;
+
 public class WordleController {
+    private final WordleGame game;
+
+    public WordleController(WordleGame game, WordleCLI cli) {
+        this.game = game;
+        cli.setController(this);
+    }
 }
--- src/main/java/stacs/wordle/model/view/WordleCLI.java
+++ src/main/java/stacs/wordle/model/view/WordleCLI.java
@@ -1,4 +1,12 @@
 package stacs.wordle.model.view;
 
+import stacs.wordle.model.controller.WordleController;
+
 public class WordleCLI {
+    public void setController(WordleController wordleController) {
+    }
+
+    public void start() {
+
+    }
 }
