656f8889dbe2022719beda84241f08ee046c91a8
poojagurubasappa <poojagurubasappa@gmail.com>
2023-02-07 23:17:11 +0000

    add scanner to game cli view

--- src/main/java/stacs/wordle/view/WordleCLI.java
+++ src/main/java/stacs/wordle/view/WordleCLI.java
@@ -2,11 +2,36 @@ package stacs.wordle.view;
 
 import stacs.wordle.controller.WordleController;
 
+import java.util.Scanner;
+
 public class WordleCLI {
+    private WordleController wordleController;
+
     public void setController(WordleController wordleController) {
+        this.wordleController = wordleController;
     }
 
     public void start() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        this.displayGameInstructions();
+        this.displayGameBoard();
+        while (scanner.hasNextLine()) {
+            userInput = scanner.nextLine();
+            handleUserInput(userInput, scanner);
+        }
+        scanner.close();
+    }
+
+    private void displayGameInstructions() {
+
+    }
+
+    private void displayGameBoard() {
+
+    }
+
+    private void handleUserInput(String userInput, Scanner scanner) {
 
     }
 }
